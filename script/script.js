49b83ce54c078dcec572c1a8ab49b83054a472e5
katbosnic <katbosnic@gmail.com>
2019-05-16 11:29:19 -0400

    getting responses to show up

--- script/script.js
+++ script/script.js
@@ -5,28 +5,46 @@ const handleSubmit = () => {
 
         // get values from quiz and convert them to integers
         const answerOneValue = parseInt($('input[name=question1]:checked').val());
-        console.log(answerOneValue)
 
         const answerTwoValue = parseInt($('input[name=question2]:checked').val());
-        console.log(answerTwoValue)
 
         const answerThreeValue = parseInt($('input[name=question3]:checked').val());
-        console.log(answerThreeValue)
 
         const answerFourValue = parseInt($('input[name=question4]:checked').val());
-        console.log(answerFourValue)
 
         const answerFiveValue = parseInt($('input[name=question5]:checked').val());
-        console.log(answerFiveValue)
 
         calcUserScore(answerOneValue, answerTwoValue, answerThreeValue, answerFourValue, answerFiveValue);
     })
 }
 
+// calculate the user's score
 const calcUserScore = (one, two, three, four, five) => {
-    $(this).map()
+    const total = one + two + three + four + five;
+    playerMatchup(total);
+
 }
 
+// match the score with the players
+const playerMatchup = (total) => {
+    if (total < 4) {
+        $('.quiz').addClass('hidden');
+        $('.resultDug').addClass('displayResult');
+    } else if (total >= 4 && total < 8) {
+        $('.quiz').addClass('hidden');
+        $('.resultFred').addClass('displayResult')
+    } else if (total >= 8 && total < 12) {
+        $('.quiz').addClass('hidden');
+        $('.resultKyle').addClass('displayResult')
+    } else if (total >= 12 && total < 16) {
+        $('.quiz').addClass('hidden');
+        $('.resultKawhi').addClass('displayResult')
+    } else if (total >= 16) {
+        $('.quiz').addClass('hidden');
+        $('.resultPascal').addClass('displayResult')
+    }
+
+}
 
 
 
